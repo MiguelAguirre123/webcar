921f5cd604000298f870a2fe6904b06f3c4ceaa1
JuanDiego500 <jdgiraldo98535@umanizales.edu.co>
2024-05-10 10:48:13 -0500

    changes in get communities

--- src/Controllers/communityController.js
+++ src/Controllers/communityController.js
@@ -1,118 +1,13 @@
 require('express');
 const community = require('../Models/community');
 
-async function createCommunity(req, res){
-    try{
+async function createCommunity(req, res) {
+    try {
         await community.create({
             communityName: req.body.communityName,
             communityCreator: req.body.communityCreator,
             communityDescription: req.body.communityDescription
-        }).then(function(data){
-            return res.status(200).json({
-                data: data
-            });
-        }).catch(error =>{
-            return res.status(400).json({
-                error: error
-            });
-        })
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-async function listCommunity(req , res){
-    try{
-        await community.findAll({
-            attributes: [
-            'communityName',
-            'communityCreator',
-            'communityDescription'
-            ],
-            order: ['communityName']
-        }).then(function(data){
-            return res.status(200).json({
-                data:data
-            });
-        }).catch(error =>{
-            return res.status(400).json({
-                error: error
-            });
-
-        })
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-async function updateCommunity(req,res){
-    try {
-        await community.update({
-            communityName: req.body.communityName,
-            communityDescription: req.body.communityDescription
-        },{
-            where: {communityId: req.params.communityId }
-        }).then(function(data){
-            return res.status(200).json({
-                data:data
-            });
-        }).catch(error =>{
-            return res.status(400).json({
-                error: error
-            });
-
-        })
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-async function disableCommunity(req,res){
-    try {
-        await community.destroy({
-            where: {communityId: req.params.communityId}
-        }).then(function(data){
-            return res.status(200).json({
-                data:data
-            });
-        }).catch(error =>{
-            return res.status(400).json({
-                error: error
-            });
-
-        })
-    } catch (e) {
-        console.log(e);
-    }
-}
-async function enableCommunity(req,res){
-    try {
-        await community.restore({
-            where: {communityId: req.params.communityId}
-        }).then(function(data){
-            return res.status(200).json({
-                data:data
-            });
-        }).catch(error =>{
-        return res.status(400).json({
-            error: error
-            });
-        })
-    } catch (e) {
-    console.log(e);
-    }
-}
-async function getCommunity(req, res){
-    try{
-        await community.findOne({
-            where: {communityId : req.params.communityId},
-            attributes: [
-                'communityId',
-                'communityName',
-                'communityCreator',
-                'communityDescription'
-            ],
-        }).then(function (data){
+        }).then(function (data) {
             return res.status(200).json({
                 data: data
             });
@@ -122,7 +17,113 @@ async function getCommunity(req, res){
             });
         })
     }
-    catch(e){
+    catch (e) {
+        console.log(e);
+    }
+}
+async function listCommunity(req, res) {
+    try {
+        await community.findAll({
+            attributes: [
+                'communityId',
+                'communityName',
+                'communityCreator',
+                'communityDescription'
+            ],
+            order: ['communityName']
+        }).then(function (data) {
+            return res.status(200).json({
+                data: data
+            });
+        }).catch(error => {
+            return res.status(400).json({
+                error: error
+            });
+
+        })
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+async function updateCommunity(req, res) {
+    try {
+        await community.update({
+            communityName: req.body.communityName,
+            communityDescription: req.body.communityDescription
+        }, {
+            where: { communityId: req.params.communityId }
+        }).then(function (data) {
+            return res.status(200).json({
+                data: data
+            });
+        }).catch(error => {
+            return res.status(400).json({
+                error: error
+            });
+
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function disableCommunity(req, res) {
+    try {
+        await community.destroy({
+            where: { communityId: req.params.communityId }
+        }).then(function (data) {
+            return res.status(200).json({
+                data: data
+            });
+        }).catch(error => {
+            return res.status(400).json({
+                error: error
+            });
+
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+async function enableCommunity(req, res) {
+    try {
+        await community.restore({
+            where: { communityId: req.params.communityId }
+        }).then(function (data) {
+            return res.status(200).json({
+                data: data
+            });
+        }).catch(error => {
+            return res.status(400).json({
+                error: error
+            });
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+async function getCommunity(req, res) {
+    try {
+        await community.findOne({
+            where: { communityId: req.params.communityId },
+            attributes: [
+                'communityId',
+                'communityName',
+                'communityCreator',
+                'communityDescription'
+            ],
+        }).then(function (data) {
+            return res.status(200).json({
+                data: data
+            });
+        }).catch(error => {
+            return res.status(400).json({
+                error: error
+            });
+        })
+    }
+    catch (e) {
         console.log(e);
     }
 }
