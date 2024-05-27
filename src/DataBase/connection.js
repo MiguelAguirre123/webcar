d736d63fa9bfe4739fd3ee7555a759b8d8ed7df8
EdwinAndres119 <125221072+EdwinAndres119@users.noreply.github.com>
2024-05-26 21:52:36 -0500

    Customer and product

--- src/DataBase/connection.js
+++ src/DataBase/connection.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 var dataBase = 'webCar'; 
 var userName = 'postgres'; 
-var password = 'Luismi0522!'; 
+var password = 'postgres'; 
 
 const connection = new Sequelize(dataBase, userName, password, { 
     host: 'localhost', 
